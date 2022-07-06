b26696a8fff420f94ffe2b9243a15106fba00292
Kai Takami <kaitakami@protonmail.com>
2022-07-06 17:19:56 +0900

    Modify output of webpack.config.js for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,37 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"]
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      }
-    ]
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: 'src/index.html' })
-  ],
-  devServer: {
-    open: true,
-    port: 3000
-  },
+const ruleForStyles = {
+  test: /\.css$/,
+  use: ["style-loader", "css-loader", "postcss-loader"]
+}
+
+const ruleForHtml = {
+  test: /\.html$/i,
+  loader: "html-loader"
+}
+
+module.exports = (env, argv) => {
+  const { mode } = argv
+  const isProduction = mode === 'production'
+  return {
+    // entry: './src/index.js',
+    output: {
+      filename: isProduction ? '[name].[contenthash].js' : 'main.js',
+      path: path.resolve(__dirname, "build")
+    },
+    module: {
+      rules: [
+        ruleForStyles,
+        ruleForHtml,
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: 'src/index.html' })
+    ],
+    devServer: {
+      open: true,
+      port: 3000
+    },
+  }
 }
